5c07fe6698c4fc01edce83bea6ba44b6810613ca
SirEnexx <113475760+SirEnexx@users.noreply.github.com>
2022-10-04 16:40:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+function f1() {
+    var a = document.getElementsByClassName('false')[0]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f2() {
+    var a = document.getElementsByClassName('false')[1]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f3() {
+    var a = document.getElementsByClassName('false')[2]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f4() {
+    var a = document.getElementsByClassName('true')[0]
+    a.style.backgroundColor = 'black'
+}
+
+
+function f5() {
+    var a = document.getElementsByClassName('false')[3]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f6() {
+    var a = document.getElementsByClassName('false')[4]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f7() {
+    var a = document.getElementsByClassName('true')[1]
+    a.style.backgroundColor = 'black'
+}
+function f8() {
+    var a = document.getElementsByClassName('false')[5]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+
+
+function f9() {
+    var a = document.getElementsByClassName('true')[2]
+    a.style.backgroundColor = 'black'
+}
+function f10() {
+    var a = document.getElementsByClassName('false')[6]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f11() {
+    var a = document.getElementsByClassName('false')[7]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f12() {
+    var a = document.getElementsByClassName('false')[8]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+
+
+function f13() {
+    var a = document.getElementsByClassName('false')[9]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f14() {
+    var a = document.getElementsByClassName('false')[10]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f15() {
+    var a = document.getElementsByClassName('true')[3]
+    a.style.backgroundColor = 'black'
+}
+function f16() {
+    var a = document.getElementsByClassName('false')[11]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+
+
+function f17() {
+    var a = document.getElementsByClassName('true')[4]
+    a.style.backgroundColor = 'black'
+}
+function f18() {
+    var a = document.getElementsByClassName('false')[12]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f19() {
+    var a = document.getElementsByClassName('false')[13]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f20() {
+    var a = document.getElementsByClassName('false')[14]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+
+
+function f21() {
+    var a = document.getElementsByClassName('true')[5]
+    a.style.backgroundColor = 'black'
+}
+function f22() {
+    var a = document.getElementsByClassName('false')[15]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f23() {
+    var a = document.getElementsByClassName('false')[16]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
+function f24() {
+    var a = document.getElementsByClassName('false')[17]
+    a.style.backgroundColor = 'red'
+    a.style.color = 'black'
+}
